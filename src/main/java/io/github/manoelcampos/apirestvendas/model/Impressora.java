48d59e5816a3c8331d68698c59e8029c0e839d75
Manoel Campos <manoelcampos@gmail.com>
2023-11-22 13:14:19 -0300

    Cria classe Impressora
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/java/io/github/manoelcampos/apirestvendas/model/Impressora.java
@@ -0,0 +1,13 @@
+package io.github.manoelcampos.apirestvendas.model;
+
+/**
+ * - Criar main
+ * - Chamar método imprimir() e alt+enter pra criá-lo
+ * - Usar List.of pra criar uma lista e imprimir
+ * - Introduzir parâmetro para a lista
+ * - Chamar imprimir passando diferentes listas
+ * @author Manoel Campos
+ */
+public class Impressora {
+
+}
